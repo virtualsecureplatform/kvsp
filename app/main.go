845dfc4594e937ac4c9d2d504e34cec018e85986
Naoki Matsumoto <m.naoki9911@gmail.com>
2019-12-10 22:54:25 +0900

    Add Clang option -nostdlib

--- app/main.go
+++ app/main.go
@@ -460,7 +460,7 @@ func doCC() error {
 	}
 
 	// Run
-	return execCmd(path, append(os.Args[2:], "-target", "cahp", "-Oz", "--sysroot", cahpRtPath))
+	return execCmd(path, append(os.Args[2:], "-target", "cahp", "-Oz", "-nostdlib", "--sysroot", cahpRtPath))
 }
 
 func doEmu() error {
