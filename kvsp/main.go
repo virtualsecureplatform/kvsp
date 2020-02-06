ac258199a8032e5593ccbf1cbff3000fd72a5fc9
Ushitora Anqou <ushitora@anqou.net>
2020-02-06 20:04:31 +0900

    [kvsp] Update for new components

--- kvsp/main.go
+++ kvsp/main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"debug/elf"
-	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"runtime"
-	"strconv"
 	"strings"
 )
 
@@ -62,20 +57,22 @@ func getPathOf(name string) (string, error) {
 			(this executable's) directory, and others are in ../share/kvsp.
 		*/
 		switch name {
-		case "CLANG":
-			path = "clang"
-		case "TFHEUTIL":
-			path = "tfheutil"
-		case "CAHP_SIM":
-			path = "cahp-sim"
 		case "CAHP_RT":
 			path = "../share/kvsp/cahp-rt"
-		case "IYOKANL2":
-			path = "iyokanl2"
-		case "IYOKANL2_CUFHE":
-			path = "iyokanl2-cufhe"
+		case "CAHP_SIM":
+			path = "cahp-sim"
+		case "CLANG":
+			path = "clang"
+		case "IYOKAN":
+			path = "iyokan"
+		case "KVSP-PACKET":
+			path = "kvsp-packet"
 		case "VSPCORE":
 			path = "../share/kvsp/vsp-core.json"
+		case "VSPCORE-WO-RAM-ROM":
+			path = "../share/kvsp/vsp-core-wo-ram-rom.json"
+		case "VSPCORE-WO-ROM":
+			path = "../share/kvsp/vsp-core-wo-rom.json"
 		default:
 			return "", errors.New("Invalid name")
 		}
@@ -112,305 +109,62 @@ func execCmd(name string, args []string) error {
 	return cmd.Run()
 }
 
-func writePlainPacket(w io.Writer, inputElfFileName string) error {
-	// Parse input ELF.
-	rom, ram, err := parseELF(inputElfFileName)
+func writePlainPacket(inputFileName, outputFileName string) error {
+	// Get the path of kvsp-packet
+	path, err := getPathOf("KVSP-PACKET")
 	if err != nil {
 		return err
 	}
 
-	// Create a KVSP plain request packet to write in.
-	packet := KVSPPlainReqPacket{
-		KVSPPlainReqPacketHeader{
-			[4]byte{'K', 'V', 'S', 'P'},
-			0,
-			uint64(len(rom)),
-			uint64(len(ram)),
-		},
-		rom,
-		ram,
-	}
-
-	_, err = packet.WriteTo(w)
-	return err
+	// Run
+	return execCmd(path, []string{"plain", inputFileName, outputFileName})
 }
 
-func getCloudKey(keyFileName string) ([]byte, error) {
-	// Get the path of tfheutil
-	tfheutilPath, err := getPathOf("TFHEUTIL")
-	if err != nil {
-		return nil, err
-	}
-
-	// Run tfheutil to get the cloud key
-	cloudKeyTmpFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(cloudKeyTmpFile.Name())
-	err = execCmd(
-		tfheutilPath,
-		[]string{"cloudkey", keyFileName, cloudKeyTmpFile.Name()})
-	if err != nil {
-		return nil, err
-	}
-
-	return ioutil.ReadAll(cloudKeyTmpFile)
-}
-
-func encryptBit(keyFileName string, bit bool) ([]byte, error) {
-	// Get the path of tfheutil
-	tfheutilPath, err := getPathOf("TFHEUTIL")
-	if err != nil {
-		return nil, err
-	}
-
-	// Write the source binary to a temporary file
-	plainTmpFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(plainTmpFile.Name())
-	if bit {
-		plainTmpFile.Write([]byte{1})
-	} else {
-		plainTmpFile.Write([]byte{0})
-	}
-	plainTmpFile.Close()
-
-	// Encrypt the binary
-	encTmpFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(encTmpFile.Name())
-	err = execCmd(
-		tfheutilPath,
-		[]string{"enc", keyFileName, plainTmpFile.Name(), encTmpFile.Name(), strconv.Itoa(1)})
-	if err != nil {
-		return nil, err
-	}
-
-	return ioutil.ReadAll(encTmpFile)
-}
-
-func encryptBytes(keyFileName string, plain []byte) ([]byte, error) {
-	// Get the path of tfheutil
-	tfheutilPath, err := getPathOf("TFHEUTIL")
-	if err != nil {
-		return nil, err
-	}
-
-	// Write the source binary to a temporary file
-	plainTmpFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(plainTmpFile.Name())
-	plainTmpFile.Write(plain)
-	plainTmpFile.Close()
-
-	// Encrypt the binary
-	encTmpFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(encTmpFile.Name())
-	err = execCmd(
-		tfheutilPath,
-		[]string{"enc", keyFileName, plainTmpFile.Name(), encTmpFile.Name(), strconv.Itoa(-1)})
-	if err != nil {
-		return nil, err
-	}
-
-	return ioutil.ReadAll(encTmpFile)
-}
-
-func decryptBit(keyFileName string, enc []byte) (bool, error) {
-	// Get the path of tfheutil
-	tfheutilPath, err := getPathOf("TFHEUTIL")
-	if err != nil {
-		return false, err
-	}
-
-	// Write the source binary to a temporary file
-	encTmpFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return false, err
-	}
-	defer os.Remove(encTmpFile.Name())
-	encTmpFile.Write(enc)
-	encTmpFile.Close()
-
-	// Decrypt the binary
-	decTmpFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return false, err
-	}
-	defer os.Remove(decTmpFile.Name())
-	err = execCmd(
-		tfheutilPath,
-		[]string{"dec", keyFileName, encTmpFile.Name(), decTmpFile.Name(), strconv.Itoa(1)})
-	if err != nil {
-		return false, err
-	}
-
-	// Read the result
-	plain, err := ioutil.ReadAll(decTmpFile)
-	if err != nil {
-		return false, err
-	}
-	if len(plain) != 1 {
-		return false, errors.New("Invalid result of tfheutil")
-	}
-
-	return (plain[0] & 1) != 0, nil
-}
-
-func decryptBytes(keyFileName string, enc []byte) ([]byte, error) {
-	// Get the path of tfheutil
-	tfheutilPath, err := getPathOf("TFHEUTIL")
-	if err != nil {
-		return nil, err
-	}
-
-	// Write the source binary to a temporary file
-	encTmpFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(encTmpFile.Name())
-	encTmpFile.Write(enc)
-	encTmpFile.Close()
-
-	// Decrypt the binary
-	decTmpFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(decTmpFile.Name())
-	err = execCmd(
-		tfheutilPath,
-		[]string{"dec", keyFileName, encTmpFile.Name(), decTmpFile.Name(), strconv.Itoa(-1)})
-	if err != nil {
-		return nil, err
-	}
-
-	return ioutil.ReadAll(decTmpFile)
-}
-
-// Parse the input as ELF and get ROM and RAM images.
-func parseELF(fileName string) ([]byte, []byte, error) {
-	input, err := elf.Open(fileName)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	rom := make([]byte, 512)
-	ram := make([]byte, 512)
-
-	for _, prog := range input.Progs {
-		addr := prog.ProgHeader.Vaddr
-		size := prog.ProgHeader.Filesz
-		if size == 0 {
-			continue
-		}
-
-		var mem []byte
-		if addr < 0x10000 { // ROM
-			mem = rom[addr : addr+size]
-		} else { // RAM
-			mem = ram[addr-0x10000 : addr-0x10000+size]
-		}
-
-		reader := prog.Open()
-		_, err := reader.Read(mem)
-		if err != nil {
-			return nil, nil, err
-		}
-	}
-
-	return rom, ram, nil
-}
-
-func runIyokanl2(args ...string) error {
-	// Get the path of iyokanl2.
-	iyokanl2Path, err := getPathOf("IYOKANL2")
+func runKVSPPacket(args ...string) error {
+	// Get the path of kvsp-packet
+	path, err := getPathOf("KVSP-PACKET")
 	if err != nil {
 		return err
 	}
 
-	// Get the path of VSP core.
-	vspcorePath, err := getPathOf("VSPCORE")
-	if err != nil {
-		return err
-	}
-
-	// Set #CPUs plus 1 as #threads of iyokanl2.
-	numThreads := runtime.NumCPU() + 1
-
-	// Run the encrypted program as is.
-	return execCmd(iyokanl2Path, append([]string{
-		"-t", fmt.Sprint(numThreads),
-		"-l", vspcorePath,
-	}, args...))
+	// Run
+	return execCmd(path, args)
 }
 
-func runIyokanl2CuFHE(args ...string) error {
-	// Get the path of iyokanl2-cufhe.
-	iyokanl2Path, err := getPathOf("IYOKANL2_CUFHE")
+func runIyokan(enableRAM, enableROM bool, args ...string) error {
+	// Get the path of iyokan.
+	iyokanPath, err := getPathOf("IYOKAN")
 	if err != nil {
 		return err
 	}
 
-	// Get the path of VSP core.
-	vspcorePath, err := getPathOf("VSPCORE")
+	if enableRAM {
+		args = append(args, "--enable-ram")
+	}
+	if enableROM {
+		args = append(args, "--enable-rom", "io_romAddr:7:io_romData:32")
+	}
+
+	pathVSPCore := ""
+	if enableRAM && enableROM {
+		pathVSPCore, err = getPathOf("VSPCORE-WO-RAM-ROM")
+	}
+	if !enableRAM && enableROM {
+		pathVSPCore, err = getPathOf("VSPCORE-WO-ROM")
+	}
+	if !enableRAM && !enableROM {
+		pathVSPCore, err = getPathOf("VSPCORE")
+	}
 	if err != nil {
 		return err
 	}
-
-	// Run the encrypted program as is.
-	return execCmd(iyokanl2Path, append([]string{
-		"-l", vspcorePath,
-	}, args...))
-}
-
-func printRes(flags []bool, regs []uint16, ram []uint8) error {
-	for i, flag := range flags {
-		fmt.Printf("Flag %d : %t\n", i, flag)
+	if pathVSPCore == "" {
+		return errors.New("Invalid spec of RAM and ROM")
 	}
-	fmt.Printf("\n")
-	for i, reg := range regs {
-		fmt.Printf("Reg %d : %d\n", i, reg)
-	}
-	fmt.Printf("\nRAM :\n")
-	for i, b := range ram {
-		fmt.Printf("%02x ", b)
-		if i%16 == 15 {
-			fmt.Printf("\n")
-		}
-	}
-	fmt.Printf("\n")
+	args = append(args, "-l", pathVSPCore)
 
-	return nil
-}
-
-func printResAsJSON(flags []bool, regs []uint16, ram []uint8) error {
-	ramReadable := make([]int, len(ram))
-	for i, b := range ram {
-		ramReadable[i] = int(b)
-	}
-	json, err := json.Marshal(struct {
-		Flags []bool
-		Regs  []uint16
-		RAM   []int
-	}{flags, regs, ramReadable})
-	if err != nil {
-		return err
-	}
-	os.Stdout.Write(json)
-
-	return nil
+	// Run iyokan
+	return execCmd(iyokanPath, args)
 }
 
 func doCC() error {
@@ -453,42 +207,21 @@ func doEmu() error {
 		return err
 	}
 	defer os.Remove(reqTmpFile.Name())
-	err = writePlainPacket(reqTmpFile, fileName)
+	err = writePlainPacket(fileName, reqTmpFile.Name())
 	if err != nil {
 		return err
 	}
 
-	// Run iyokanl2 to get emulation result.
-	resTmpFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return err
-	}
-	defer os.Remove(resTmpFile.Name())
-	err = runIyokanl2("-o", resTmpFile.Name(), "-p", reqTmpFile.Name())
-	if err != nil {
-		return err
-	}
-
-	// Read the result.
-	resPacket := KVSPPlainResPacket{}
-	_, err = resPacket.ReadFrom(resTmpFile)
-	if err != nil {
-		return err
-	}
-
-	// Print result
-	printRes(resPacket.Flags, resPacket.Regs, resPacket.RAM)
-
-	return nil
+	// Run iyokan to get emulation result.
+	return runIyokan(true, true, "plain", "-i", reqTmpFile.Name())
 }
 
 func doDec() error {
 	// Parse command-line arguments.
 	fs := flag.NewFlagSet("dec", flag.ExitOnError)
 	var (
-		shouldOutputJSON = fs.Bool("json", false, "Print results as JSON")
-		keyFileName      = fs.String("k", "", "Key file name")
-		inputFileName    = fs.String("i", "", "Input file name (encrypted)")
+		keyFileName   = fs.String("k", "", "Key file name")
+		inputFileName = fs.String("i", "", "Input file name (encrypted)")
 	)
 	err := fs.Parse(os.Args[2:])
 	if err != nil {
@@ -498,54 +231,8 @@ func doDec() error {
 		return errors.New("Specify -k and -i options properly")
 	}
 
-	// Do decryption.
-	reader, err := os.Open(*inputFileName)
-	if err != nil {
-		return err
-	}
-	packet := KVSPResPacket{}
-	_, err = packet.ReadFrom(reader)
-	if err != nil {
-		return err
-	}
-
-	// Decrypt flags
-	flags := make([]bool, packet.Nflags)
-	for i, encFlag := range packet.Flags {
-		flags[i], err = decryptBit(*keyFileName, encFlag)
-		if err != nil {
-			return err
-		}
-	}
-
-	// Decrypt regs
-	regs := make([]uint16, packet.Nregs)
-	for i, encReg := range packet.Regs {
-		plainReg, err := decryptBytes(*keyFileName, encReg)
-		if err != nil {
-			return err
-		}
-		regs[i] = uint16(plainReg[0]) | (uint16(plainReg[1]) << 8)
-	}
-
-	// Decrypt RAM
-	ram, err := decryptBytes(*keyFileName, packet.RAM)
-	if err != nil {
-		return err
-	}
-
-	// Print the result as JSON
-	if *shouldOutputJSON {
-		err = printResAsJSON(flags, regs, ram)
-	} else {
-		// Print the result
-		err = printRes(flags, regs, ram)
-	}
-	if err != nil {
-		return err
-	}
-
-	return nil
+	// Run kvsp-packet
+	return runKVSPPacket("dec", *keyFileName, *inputFileName)
 }
 
 func doEnc() error {
@@ -564,53 +251,8 @@ func doEnc() error {
 		return errors.New("Specify -k, -i, and -o options properly")
 	}
 
-	// Parse input ELF.
-	rom, ram, err := parseELF(*inputFileName)
-	if err != nil {
-		return err
-	}
-
-	// Encrypt the images
-	romEnc, err := encryptBytes(*keyFileName, rom)
-	if err != nil {
-		return err
-	}
-	ramEnc, err := encryptBytes(*keyFileName, ram)
-	if err != nil {
-		return err
-	}
-
-	// Get the cloud key from the secret one
-	cloudKey, err := getCloudKey(*keyFileName)
-	if err != nil {
-		return err
-	}
-
-	// Create a KVSP request packet to write in
-	packet := KVSPReqPacket{
-		KVSPReqPacketHeader{
-			[4]byte{'K', 'V', 'S', 'P'},
-			0,
-			uint64(len(cloudKey)),
-			uint64(len(romEnc)),
-			uint64(len(ramEnc)),
-		},
-		cloudKey,
-		romEnc,
-		ramEnc,
-	}
-
-	// Write the packet to the output
-	writer, err := os.Create(*outputFileName)
-	if err != nil {
-		return err
-	}
-	_, err = packet.WriteTo(writer)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	// Run kvsp-packet
+	return runKVSPPacket("enc", *keyFileName, *inputFileName, *outputFileName)
 }
 
 func doGenkey() error {
@@ -627,14 +269,7 @@ func doGenkey() error {
 		return errors.New("Specify -o options properly")
 	}
 
-	// Get the path of tfheutil
-	path, err := getPathOf("TFHEUTIL")
-	if err != nil {
-		fatalExit(err)
-	}
-
-	// Generate a key.
-	return execCmd(path, []string{"genkey", *outputFileName})
+	return runKVSPPacket("genkey", *outputFileName)
 }
 
 func doPlainpacket() error {
@@ -652,17 +287,7 @@ func doPlainpacket() error {
 		return errors.New("Specify -i, and -o options properly")
 	}
 
-	// Write the packet to the output
-	writer, err := os.Create(*outputFileName)
-	if err != nil {
-		return err
-	}
-	err = writePlainPacket(writer, *inputFileName)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return runKVSPPacket("plain", *inputFileName, *outputFileName)
 }
 
 func doRun() error {
@@ -682,12 +307,17 @@ func doRun() error {
 		return errors.New("Specify -c, -i, and -o options properly")
 	}
 
+	args := []string{
+		"tfhe",
+		"-i", *inputFileName,
+		"-o", *outputFileName,
+		"-c", fmt.Sprint(*nClocks),
+	}
 	if *isGPU {
-		return runIyokanl2CuFHE(
-			"-o", *outputFileName, "-i", *inputFileName, "-c", fmt.Sprint(*nClocks))
+		args = append(args, "--enable-gpu")
+		return runIyokan(false, false, args...)
 	} else {
-		return runIyokanl2(
-			"-o", *outputFileName, "-i", *inputFileName, "-c", fmt.Sprint(*nClocks))
+		return runIyokan(true, true, args...)
 	}
 }
 
